be016d62c635b286ca9028c648cb409ac8a7bd13
khusainovmehrubon <husaynov888@gmail.com>
2025-11-12 15:43:28 +0500

    Implement Top10 function for frequency analysis of words

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var splitPattern = regexp.MustCompile(`[\s!.,?]+`)
+
+type frequencyWords struct {
+	word      string
+	frequency int
+}
+
+func Top10(str string) []string {
+	// Place your code here
+	countWords := wordFrequencyAnalysis(str)
+	sort.Slice(countWords, func(i, j int) bool {
+		return countWords[i].frequency > countWords[j].frequency
+	})
+
+	return topResult(countWords, 10)
+}
+
+func wordFrequencyAnalysis(str string) []frequencyWords {
+	words := wordsCounter(str)
+	wordsFreq := make([]frequencyWords, 0, len(words))
+	for word, count := range words {
+		wordsFreq = append(wordsFreq, frequencyWords{word: word, frequency: count})
+	}
+
+	return wordsFreq
+}
+
+func wordsCounter(str string) map[string]int {
+	wordCount := make(map[string]int)
+	for _, word := range splitPattern.Split(str, -1) {
+		if word == "" || word == "-" {
+			continue
+		}
+		word = strings.ToLower(word)
+		wordCount[word]++
+	}
+
+	return wordCount
+}
+
+func topResult(words []frequencyWords, n int) []string {
+	if n > len(words) {
+		n = len(words)
+	}
+	result := make([]string, 0, n)
+	for _, word := range words[:n] {
+		result = append(result, word.word)
+	}
+	return result
 }
