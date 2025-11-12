8f65cbb0f71188c4e6e0ce08ea00452d56cbcc12
khusainovmehrubon <husaynov888@gmail.com>
2025-11-12 16:15:40 +0500

    Update module path in go.mod for hw-test relocation

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,58 +1,41 @@
 package hw03frequencyanalysis
 
 import (
-	"regexp"
 	"sort"
 	"strings"
 )
 
-var splitPattern = regexp.MustCompile(`[\s!.,?]+`)
-
-type frequencyWords struct {
-	word      string
-	frequency int
+type keyVal struct {
+	word  string
+	count int
 }
 
-func Top10(str string) []string {
-	// Place your code here
-	countWords := wordFrequencyAnalysis(str)
-	sort.Slice(countWords, func(i, j int) bool {
-		return countWords[i].frequency > countWords[j].frequency
+func Top10(inputText string) []string {
+	words := strings.Fields(inputText)
+
+	wordCnt := make(map[string]int)
+	for _, w := range words {
+		wordCnt[w]++
+	}
+
+	counts := make([]keyVal, 0, len(wordCnt))
+	for w, c := range wordCnt {
+		counts = append(counts, keyVal{w, c})
+	}
+
+	sort.Slice(counts, func(i, j int) bool {
+		if counts[i].count == counts[j].count {
+			return counts[i].word < counts[j].word
+		}
+		return counts[i].count > counts[j].count
 	})
 
-	return topResult(countWords, 10)
-}
+	outputLen := min(10, len(counts))
 
-func wordFrequencyAnalysis(str string) []frequencyWords {
-	words := wordsCounter(str)
-	wordsFreq := make([]frequencyWords, 0, len(words))
-	for word, count := range words {
-		wordsFreq = append(wordsFreq, frequencyWords{word: word, frequency: count})
+	output := make([]string, outputLen)
+	for i := 0; i < outputLen; i++ {
+		output[i] = counts[i].word
 	}
 
-	return wordsFreq
-}
-
-func wordsCounter(str string) map[string]int {
-	wordCount := make(map[string]int)
-	for _, word := range splitPattern.Split(str, -1) {
-		if word == "" || word == "-" {
-			continue
-		}
-		word = strings.ToLower(word)
-		wordCount[word]++
-	}
-
-	return wordCount
-}
-
-func topResult(words []frequencyWords, n int) []string {
-	if n > len(words) {
-		n = len(words)
-	}
-	result := make([]string, 0, n)
-	for _, word := range words[:n] {
-		result = append(result, word.word)
-	}
-	return result
+	return output
 }
